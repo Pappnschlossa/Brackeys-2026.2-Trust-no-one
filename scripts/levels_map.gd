b2dda7da24fb25fc2c1bedf4a01c5603227d884e
Pappnschlossa <thibaut.vebret@free.fr>
2026-08-28 16:52:37 +0200

    Added map and chains animation

--- scripts/levels_map.gd
+++ scripts/levels_map.gd
@@ -2,5 +2,5 @@ extends Control
 
 
 func _ready() -> void:
-	get_node("VBox/Text").text = "Level %s" % str(g.level) # Temp
+	get_node("VBox/Text").text = "Floor %s" % str(g.level)
 	get_node("MapCursor").position.y = g.level*(1080-128)/50
